ca45dda59d7858d339a726d03e1df7888a2528d6
Radwan Ahmed <radwan.hussein@protonmail.com>
2023-01-16 09:15:54 -0500

    commit

---
+++ pages/RadCamp.js
@@ -0,0 +1,80 @@
+import { Box, Circle, Heading, Center, position } from "@chakra-ui/react";
+import React from "react";
+import ReactPlayer from "react-player";
+import { Image, Button } from "@chakra-ui/react";
+import { useColorModeValue } from "@chakra-ui/react";
+import NextLink from "next/link";
+
+export default function Radsocial() {
+  return (
+    <div>
+      <Center>
+        <Box w={{ base: "100%", sm: "60%" }} ml="5%" mr="5%" mb="5%">
+          <Box display={"flex"} flexDirection={{ base: "column", lg: "row" }}>
+            <Box
+              id="logo"
+              style={{
+                maxWidth: "200px",
+                minWidth: "200px",
+                height: "200px",
+                margin: "0 auto",
+                textAlign: "center",
+              }}
+            >
+              <Image
+                src="/bonfire.png"
+                height={"120px"}
+                width="120px"
+                alt="bonfire image"
+                margin="0 auto"
+              />
+              <Heading as="h4" fontSize="35px" paddingLeft={'10px'} variant="page-title">
+                RadCamp
+              </Heading>
+            </Box>
+            <Box mb="10%" p={"40px"}>
+              <Box
+                mt="20px"
+                p={3}
+                bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+                css={{ backdropFilter: "blur(10px)" }}
+                rounded="xl"
+                textAlign={"center"}
+              >
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  porttitor accumsan augue nec ultrices. Lorem ipsum dolor sit
+                  amet, consectetur adipiscing elit. Sed porttitor accumsan
+                  augue nec ultrices. Lorem ipsum dolor sit amet, consectetur
+                  adipiscing elit. Sed porttitor accumsan augue nec ultrices.
+                </p>
+              </Box>
+            </Box>
+          </Box>
+          <Box>
+            <Box pt={"10px"} marginX="5%">
+              <Center>
+                <Heading as="h4" pb="20px" fontSize="30px">
+                  Tutorial
+                </Heading>
+              </Center>
+              <video controls>
+                <source
+                  src="https://res.cloudinary.com/cattleherd/video/upload/v1673560904/reactsocialmediaAssets/2023-01-09_10-42-00_hvv0cc.mp4"
+                  type="video/mp4"
+                />
+              </video>
+            </Box>
+            <NextLink href="https://rad-social.herokuapp.com" passHref>
+              <Center>
+                <Button mt={"40px"} mb={"40px"} w="150px">
+                  Demo
+                </Button>
+              </Center>
+            </NextLink>
+          </Box>
+        </Box>
+      </Center>
+    </div>
+  );
+}
