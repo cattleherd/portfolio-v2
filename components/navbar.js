42ef3624639f52e7a480d6e984a1e4b95b15dc75
Radwan Ahmed <radwan.hussein@protonmail.com>
2023-01-16 08:08:10 -0500

    fix routing

--- components/navbar.js
+++ components/navbar.js
@@ -9,34 +9,34 @@ import {
   MenuList,
   MenuButton,
   IconButton,
-  useColorModeValue
-} from '@chakra-ui/react'
+  useColorModeValue,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
-import { HamburgerIcon } from '@chakra-ui/icons'
-import ThemeToggle from './theme-toggle-button'
-import Logo from './logo'
+import { HamburgerIcon } from "@chakra-ui/icons";
+import ThemeToggle from "./theme-toggle-button";
+import Logo from "./logo";
 
 //custom navbar link component
 const LinkItem = ({ href, children }) => {
   return (
-      //href is passed as prop to link object
-      <Link p={3} href={href}>
-        {children}
-      </Link>
-  )
-}
+    //href is passed as prop to link object
+    <Link p={3} href={href}>
+      {children}
+    </Link>
+  );
+};
 
-const Navbar = props => {
-  const { path } = props
+const Navbar = (props) => {
+  const { path } = props;
   return (
     <Box //nav wrapper, sets bg, fixed, and width
       position="fixed"
       as="nav"
       w="100%"
-      bg={useColorModeValue('#ffffff40', '#20202380')}
+      bg={useColorModeValue("#ffffff40", "#20202380")}
       zIndex={1}
     >
-      <Container //inner nav, container padding (left and right margins) 
+      <Container //inner nav, container padding (left and right margins)
         display="flex"
         p={2}
         maxW="container.md"
@@ -46,37 +46,67 @@ const Navbar = props => {
       >
         <Logo />
         <Box //Left side of nav (link items)
-          display={{ base: 'none', md: 'flex' }} paddingLeft={'50px'} paddingTop={'10px'}//hides link elements based on viewport
+          display={{ base: "none", md: "flex" }}
+          paddingLeft={"50px"}
+          paddingTop={"10px"} //hides link elements based on viewport
         >
-          <Box margin={'12px'}><NextLink href="/" passHref>About</NextLink></Box>
-          <Box margin={'12px'}><NextLink href="/Works" passHref>Works</NextLink></Box>
-          <Box margin={'12px'}><NextLink href="/ContactForm" passHref>Contact</NextLink></Box>
-          <Box margin={'12px'}><NextLink href="https://github.com/cattleherd" passHref>Github</NextLink></Box>
+          <Box margin={"12px"}>
+            <NextLink href="/" passHref>
+              About
+            </NextLink>
+          </Box>
+          <Box margin={"12px"}>
+            <NextLink href="/Works" passHref>
+              Works
+            </NextLink>
+          </Box>
+          <Box margin={"12px"}>
+            <NextLink href="/ContactForm" passHref>
+              Contact
+            </NextLink>
+          </Box>
+          <Box margin={"12px"}>
+            <NextLink href="https://github.com/cattleherd" passHref>
+              Github
+            </NextLink>
+          </Box>
         </Box>
-        
-        <Box flex={1} align="right" marginTop={'15px'} //right side of nav (theme toggle and hamburger menu)
+
+        <Box
+          flex={1}
+          align="right"
+          marginTop={"15px"} //right side of nav (theme toggle and hamburger menu)
         >
           <ThemeToggle />
-          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>{/* hamburger menu */}
+          <Box ml={2} display={{ base: "inline-block", md: "none" }}>
+            {/* hamburger menu */}
             <Menu isLazy id="hamburger-menu">
               <MenuButton
                 as={IconButton} //icon rendered as button
                 icon={<HamburgerIcon />}
                 variant="outline" //outline style for hamburger menu
-                aria-label="Options" //accessibility 
+                aria-label="Options" //accessibility
               />
               <MenuList>
-                  <MenuItem href='/'>About</MenuItem>
-                  <MenuItem href='/'>Works</MenuItem>
-                  <MenuItem href='/'>Contact</MenuItem>
-                  <MenuItem href='/'>Github</MenuItem>
+                <NextLink href="/" passHref>
+                  <MenuItem>About</MenuItem>
+                </NextLink>
+                <NextLink href="/Works">
+                  <MenuItem>Works</MenuItem>
+                </NextLink>
+                <NextLink href="/ContactForm">
+                  <MenuItem>Contact</MenuItem>
+                </NextLink>
+                <NextLink href="https://github.com/cattleherd">
+                  <MenuItem>Github</MenuItem>
+                </NextLink>
               </MenuList>
             </Menu>
           </Box>
         </Box>
       </Container>
     </Box>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
